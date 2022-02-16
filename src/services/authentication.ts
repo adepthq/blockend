45fd8f46919af00a834e070d734c8df90d976c3d
Alleo Indong <alleo.indong@gmail.com>
2022-02-16 20:50:26 +0800

    authentication service

---
+++ src/services/authentication.ts
@@ -0,0 +1,78 @@
+import moment from 'moment';
+import { Collection } from 'mongodb';
+import jwt from 'jsonwebtoken';
+import { User } from 'models/user';
+import mongoclient from '../lib/mongoclient';
+import { Auth } from '../config';
+
+const getDBCollection = async (): Promise<Collection> => {
+  const client = await mongoclient.connect();
+  const db = client.db('blockheads');
+  const collection = db.collection('users');
+
+  return collection;
+};
+
+const findUserByWalletAddress = async (walletAddress: string): Promise<User | null> => {
+  const collection = await getDBCollection();
+
+  const user = (await collection.findOne({ walletAddress })) as User;
+
+  return user;
+};
+
+const createUser = async (user: User): Promise<User> => {
+  const collection = await getDBCollection();
+
+  const newUser = await collection.insertOne(user);
+
+  return {
+    ...user,
+    _id: newUser.insertedId,
+  };
+};
+
+const generateToken = (user: User): string => {
+  const token = jwt.sign(
+    {
+      // eslint-disable-next-line no-underscore-dangle
+      id: user._id,
+      walletAddress: user.walletAddress,
+    },
+    Auth.secret,
+    {
+      expiresIn: Auth.expiresIn,
+    }
+  );
+
+  return token;
+};
+
+const authenticate = async (walletAddress: string): Promise<User | null> => {
+  // Find user by wallet address
+  let user = await findUserByWalletAddress(walletAddress);
+
+  if (!user) {
+    // Create user
+    user = await createUser({
+      name: '',
+      walletAddress,
+      email: '',
+      password: '',
+      createdAt: moment().toDate(),
+      updatedAt: moment().toDate(),
+    });
+  }
+
+  // Generate token
+  const token = generateToken(user);
+
+  return {
+    ...user,
+    token,
+  };
+};
+
+export default {
+  authenticate,
+};
