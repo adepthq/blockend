3f55c59489b32479c24610c1cf7aceb0354526f4
Alleo Indong <alleo.indong@gmail.com>
2022-02-16 19:47:28 +0800

    singleton on mongoclient

--- src/lib/mongoclient.ts
+++ src/lib/mongoclient.ts
@@ -1,17 +1,26 @@
 import { MongoClient } from 'mongodb';
 import { Database } from '../config';
 
-const connect = async (): Promise<MongoClient> => {
-  const { username, password, host, port, database } = Database;
+let client: MongoClient | null = null;
 
-  const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`;
-  const client = new MongoClient(uri);
+const connect = async (): Promise<MongoClient> => {
+  if (client) {
+    return client;
+  }
+
+  const { username, password, host, port } = Database;
+
+  const uri = `mongodb://${username}:${password}@${host}:${port}/`;
+  console.log(`Connecting to ${uri}`);
+  client = new MongoClient(uri);
 
   return client.connect();
 };
 
-const disconnect = async (client: MongoClient): Promise<void> => {
-  return client.close();
+const disconnect = async (): Promise<void> => {
+  if (client) {
+    client.close();
+  }
 };
 
 export default {
